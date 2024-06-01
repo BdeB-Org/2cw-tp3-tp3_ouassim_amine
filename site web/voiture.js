58e0afcaee635e26d1f871073c70f56ed81bb5a2
amineouch <150829765+amineouch@users.noreply.github.com>
2024-06-01 16:03:57 -0400

    Dévellopement du site mise en place du js

---
+++ site web/voiture.js
@@ -0,0 +1,32 @@
+const voitures = [
+    {
+      id: 1,
+      marque: "Infinity",
+      kilometrage : 4500,
+      modele: "G35",
+      annees: 2022,
+      coleur: "Noir",
+      prix: 25000
+    },
+    
+  {
+      id: 2,
+      marque: "Mercedes",
+      kilometrage : 1000,
+      modele: "A45 AMG",
+      annees: 2020,
+      coleur: "Gris Nardo",
+      prix: 35000
+    },
+    
+  {
+      id: 3,
+      marque: "BMW",
+      kilometrage : 200,
+      modele: "M3 pack M",
+      annees: 2023,
+      coleur: "Bleu",
+      prix: 45000
+    }
+  ];
+
